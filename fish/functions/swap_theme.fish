fd27d9b41a06124d50b2a71497aa9452fbae3df9
Mark Smith-Guerrero <msmithgu@gmail.com>
2013-12-22 08:44:58 -0500

    swap_theme fish function improvements

--- fish/functions/swap_theme.fish
+++ fish/functions/swap_theme.fish
@@ -1,4 +1,19 @@
+set swap_themedir $HOME/.oh-my-fish/themes
+
+function swap_themes
+  ls -1 $swap_themedir/
+end
+
 function swap_theme --description 'Source a fish theme by name'
-  set theme $argv[1]
-  . ~/.oh-my-fish/themes/$theme/fish_prompt.fish
+  set theme $swap_themedir/$argv[1]/fish_prompt.fish
+  if test -f "$theme"
+    source $theme
+  else
+    swap_themes
+  end
+end
+
+for t in (swap_themes)
+  set d (sed -n '/^[a-zA-Z]/p' $swap_themedir/$t/*.md | head -1)
+  complete -x -c swap_theme -a $t -d "$d"
 end
